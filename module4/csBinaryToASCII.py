741eb8bbd41b7421a35d9427279bf9589ce8950c
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-03-19 12:49:40 -0400

    Create csBinaryToASCII.py

---
+++ module4/csBinaryToASCII.py
@@ -0,0 +1,11 @@
+'''
+Understand:
+input = string binary
+output = string
+
+Plan:
+split string to individual letters in pieces of 8 bits
+rejoin string together
+'''
+
+def csBinaryToASCII(binary):
