fdbddc0e4ec8d51600e56c6fc5ae3ece4571e4ef
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-03-19 12:33:34 -0400

    Update csReverseIntegerBits.py

--- module4/csReverseIntegerBits.py
+++ module4/csReverseIntegerBits.py
@@ -14,8 +14,12 @@ Execute
 '''
 
 def csReverseIntegerBits(n):
-    if x in n: 
-        result = {0:b}.format(n)
-            int(str(x))[::-1]))
-                int('n', 2)
-    print(result)
+    return int(bin(n)[:1:-1],2)
+
+
+#def csReverseIntegerBits(n):
+    #if x in n: 
+        #result = {0:b}.format(n)
+        #int(str(x)[::-1])
+        #int('n', 2)
+    #print(result)
