7d120639819e9d446827fe95a720fa4ea83118e0
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-03-20 14:33:06 -0400

    Update csBinaryToASCII.py

--- module4/csBinaryToASCII.py
+++ module4/csBinaryToASCII.py
@@ -9,3 +9,18 @@ rejoin string together
 '''
 
 def csBinaryToASCII(binary):
+  if len(binary) == 0:
+    return binary
+ n=8
+bin_list = [binary[i:i+n] for i in range(0, len(binary), n)]
+print(bin_list)
+ret_string = ""
+
+for bit in bin_list:
+  ret_string += chr(int(bit, 2))
+ 
+return ret_string
+
+# print(bin_list)
+# print(int(bin_list[0], 2))
+# print(chr(int(bin_list[0], 2)))
