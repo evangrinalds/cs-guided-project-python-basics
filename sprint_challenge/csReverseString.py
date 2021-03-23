d7594d659e6d07498efadbfbdd19e16e15d8aa4b
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-03-23 10:38:30 -0400

    Create csReverseString.py

---
+++ sprint_challenge/csReverseString.py
@@ -0,0 +1,19 @@
+"""
+# Understand
+input string 
+output reverse string
+
+# Plan
+return chars[::-1]
+
+# Execute
+
+# Review
+don't need for loop
+
+"""
+
+
+
+def csReverseString(chars):
+    return chars[::-1]
