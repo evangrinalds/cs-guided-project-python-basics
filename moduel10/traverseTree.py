256fc627604b96927aa59d92460d092dcbcd0086
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-04-12 11:18:58 -0400

    Create traverseTree.py

---
+++ moduel10/traverseTree.py
@@ -0,0 +1,38 @@
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+
+"""
+Understand
+input: integer
+output: [1, 2, 4, 3, 5]
+
+Plan
+add the root to the queue
+for each node, add its children to the queue from left to right
+"""
+
+def traverseTree(t):
+    
+    result = []
+    queue = []
+    queue.insert(0, t) 
+    
+    while queue:
+        elmnt = queue.pop()
+        
+        if elmnt:
+            result.append(elmnt.value)
+        else:
+            continue
+            
+        if elmnt.left:
+            queue.insert(0, elmnt.left)
+        if elmnt.right:
+            queue.insert(0, elmnt.right)
+            
+    return result
