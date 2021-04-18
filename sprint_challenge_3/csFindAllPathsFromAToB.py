e39729b7dacdd480c7992ae40ae9018985d4e685
Evan G <61983308+evanpersonalbiz@users.noreply.github.com>
2021-04-18 13:24:24 -0400

    Create csFindAllPathsFromAToB.py

---
+++ sprint_challenge_3/csFindAllPathsFromAToB.py
@@ -0,0 +1,15 @@
+"""
+Understand:
+Input: graph = [[1, 2], [3],[3],[4],[]]
+Output: [[0, 1, 3, 4], [0, 2, 3, 4]]
+Plan:
+"""
+
+def csFindAllPathsFromAToB(graph):
+        def dfs(cur, path):
+            if cur == len(graph) - 1: res.append(path)
+            else:
+                for i in graph[cur]: dfs(i, path + [i])
+        res = []
+        dfs(0, [0])
+        return res
